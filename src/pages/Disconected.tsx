05a03ebb1c16706c70c32695bf43fb249bb155da
Marcela Karafizievová <marcela.karafizievova@gmail.com>
2023-09-09 17:02:14 +0200

    #43 - Edit Disconnected and NotFound pages

--- src/pages/Disconected.tsx
+++ src/pages/Disconected.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { Container } from '@mui/material';
+import { styled } from '@mui/system';
+
+import { StyledH1, StyledSmallText } from '../styled/styled';
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+`;
+
 const Disconnected: React.FC = () => {
   return (
-    <>
-      <h1>You got disconnected after 2 hour.</h1>
-      <p>To continue, you must log in again.</p>
-    </>
+    <StyledContainer>
+      <StyledH1 variant="h1">You got disconnected after 1 hour.</StyledH1>
+      <StyledSmallText>To continue, you must log in again.</StyledSmallText>
+    </StyledContainer>
   );
 };
 
